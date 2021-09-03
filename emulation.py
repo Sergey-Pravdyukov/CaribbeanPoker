456ca5d72ccba01e82c972c20d43e2dd92f6505b
Gabriel KOstadinov <gabrielkostadinov@abv.bg>
2021-09-03 23:20:14 +0300

    Added DefiningCombination and CombinationCompare methods

--- emulation.py
+++ emulation.py
@@ -3,6 +3,9 @@ from colorama import init
 import copy  
 import random
 import argparse
+import math
+from statistics import mode
+from collections import Counter
 
 # Unicode suits characters
 suits = {"spades": u'\u2660', 
@@ -10,8 +13,24 @@ suits = {"spades": u'\u2660',
          "diamonds": u'\u2666', 
          "clubs": u'\u2663'}
 
+Dictionary = dict()
+Dictionary['1']=1
+Dictionary['2']=2
+Dictionary['3']=3
+Dictionary['4']=4
+Dictionary['5']=5
+Dictionary['6']=6
+Dictionary['7']=7
+Dictionary['8']=8
+Dictionary['9']=9
+Dictionary['T']=10
+Dictionary['J']=11
+Dictionary['Q']=12
+Dictionary['K']=13
+Dictionary['A']=14
+
 def genCardDeck():
-    cardRanks = [str(x) for x in range(2, 11)] + ['J', 'Q', 'K', 'A']
+    cardRanks = [str(x) for x in range(2, 10)] + ['T','J', 'Q', 'K', 'A']
 
     cardDeckArrays = list(map(lambda y: list(map(lambda x: "".join([x, y]), cardRanks)), list(suits.values())))
     cardDeckArray = np.concatenate(cardDeckArrays).tolist()
@@ -30,17 +49,85 @@ def printCardDeck(cardDeck, message=None):
         print(color, card['rank'] + card['suit'], end=' ')
     print()
 
+def turnHandIntoList(playerHand):
+    result=[]
+    for i in playerHand:
+        if i['suit']== u'\u2660':
+            result.append(i['rank']+'S')
+        if i['suit']== u'\u2665':
+            result.append(i['rank']+'H')
+        if i['suit']== u'\u2666':
+            result.append(i['rank']+'D')
+        if i['suit']== u'\u2663':
+            result.append(i['rank']+'C')
+    return result
+
 def emulateGame(cardDeck, playersNum):
     cardsPerPlayer = 5
     workingCardDeck = copy.deepcopy(cardDeck)
     random.shuffle(workingCardDeck)
-    croupierHand = workingCardDeck[:cardsPerPlayer]
+    croupierHand = workingCardDeck[:1]
     playerHands = []
+    decisions =[]
     for i in range(playersNum):
         playerHands.append(workingCardDeck[cardsPerPlayer * (i + 1):cardsPerPlayer * (i + 2)])
     printCardDeck(croupierHand, "croupier hand")
     for (i, playerHand) in enumerate(playerHands):
         printCardDeck(playerHand, "player " + str(i) + " hand")
+    # for i in range(5):
+    #     print("Player" + str(i)+ " make your decision:")
+    #     decisions.append(input())
+    # print(decisions)
+    croupierHand = workingCardDeck[0:playersNum]
+    printCardDeck(croupierHand, "croupier hand")
+    print()
+    for ind,i in enumerate(playerHands):
+        if DefineCombination(turnHandIntoList(croupierHand))>DefineCombination(turnHandIntoList(i)):
+            print("Player"+ str(ind) + " is losing!")
+        elif DefineCombination(turnHandIntoList(croupierHand))<DefineCombination(turnHandIntoList(i)):
+            print("Player" + str(ind) + " is winning!")
+        else:
+            print("Player" + str(ind) + " is chopping!")
+
+def DefineCombination(l):
+    list2 = list()
+    for i in l:
+        list2.append(i[0])
+
+    list2 = list(map(lambda x: Dictionary[x],list2))
+    list2 = sorted(list2)[::-1]
+    c = Counter(item for item in list2)
+    list2 = sorted(list2, key = lambda x: -c[x])
+
+
+    if  set([11, 12, 13, 14, 10]) == set(list2) and len(set([l[0][1], l[1][1], l[2][1], l[3][1], l[4][1]])) == 1:
+        return '9Royal Flush' ,list2
+    if (set([14, 2, 3, 4, 5]) == set(list2) or set([6, 2, 4, 3, 5]) == set(list2) or set([6, 7, 3, 4, 5]) == set(list2) or set([7, 6, 8, 4, 5]) == set(list2) or set([7, 6, 8, 9, 5]) == set(list2) or set([7, 6, 8, 9, 10]) == set(list2) or set([7, 8, 9, 10, 11]) == set(list2) or set([11, 12, 8, 9, 10]) == set(list2) or set([11, 12, 13, 9, 10]) == set(list2) or set([11, 12, 13, 14, 10])== set(list2) ) and len(set([l[0][1],l[1][1],l[2][1],l[3][1],l[4][1]])) ==1:
+        if set([14, 2, 3, 4, 5]) == set(list2):
+            return '8Straight Flush' , [5,4,3,2,14]
+        else:
+            return '8Straight Flush' , list2
+    if len(set([l[0][0],l[1][0],l[2][0],l[3][0],l[4][0]]))==2 and list2.count(mode(list2))==4 :
+        return '7Quads' , list2
+    if len(set([l[0][0],l[1][0],l[2][0],l[3][0],l[4][0]]))==2 and list2.count(mode(list2))==3 :
+        return '6Full House' , list2
+    if len(set([l[0][1],l[1][1],l[2][1],l[3][1],l[4][1]])) ==1:
+        return '5Flush' , list2
+    if set([14, 2, 3, 4, 5]) == set(list2) or set([6, 2, 4, 3, 5]) == set(list2) or set([6, 7, 3, 4, 5]) == set(list2) or set([7, 6, 8, 4, 5]) == set(list2) or set([7, 6, 8, 9, 5]) == set(list2) or set([7, 6, 8, 9, 10]) == set(list2) or set([7, 8, 9, 10, 11]) == set(list2) or set([11, 12, 8, 9, 10]) == set(list2) or set([11, 12, 13, 9, 10]) == set(list2) or set([11, 12, 13, 14, 10])== set(list2) :
+        if set([14, 2, 3, 4, 5]) == set(list2):
+            return '4Straight' , [5 ,4 ,3 ,2 ,14]
+        else:
+            return '4Straight' ,list2
+    if len(set([l[0][0],l[1][0],l[2][0],l[3][0],l[4][0]]))==3 and list2.count(mode(list2))==3 :
+        return '3Three of a kind' , list2
+    if len(set([l[0][0],l[1][0],l[2][0],l[3][0],l[4][0]]))==3 and list2.count(mode(list2))==2 :
+        return '2Two pair'  , list2
+    if len(set([l[0][0],l[1][0],l[2][0],l[3][0],l[4][0]]))==4 :
+        return "1Pair", list2
+    if len(set([l[0][0],l[1][0],l[2][0],l[3][0],l[4][0]]))==5 and len(set([l[0][1],l[1][1],l[2][1],l[3][1],l[4][1]])) >1 :
+        return "0High Card" , list2
+
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -50,3 +137,4 @@ if __name__ == '__main__':
     cardDeck = genCardDeck()
     printCardDeck(cardDeck)
     emulateGame(cardDeck, args.playersNum)
+
